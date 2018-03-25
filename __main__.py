38a4c3ce93269f4c7412563edbbe1460a5eadb03
Travis Mattera <haggishunk@hotmail.com>
2018-03-24 18:58:49 -0700

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,22 @@
+"""Main
+
+Initializes Ripper class with supplied directory argument
+then rips, converts and renames audio files
+"""
+import argparse
+
+
+def Main():
+    # get args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', help='specify the destination for the ripped WAVs and converted MP3s')
+    args = parser.parse_args()
+
+    rip = pyrip.Ripper(loc=args.directory)
+    rip.CDRip()
+    rip.ConvertToMP3()
+    rip.NameTracks()
+    return
+
+if __name__ == '__main__':
+    Main()
